72f0ef4e7a2584be92c3d9001e71363faa0c7810
User <user@vagelos-ve735-0711.apnd.wlan.private.upenn.edu>
2018-06-05 14:17:07 -0400

    addressbook

---
+++ python/address.py
@@ -0,0 +1,2 @@
+address={"name":"Brandon Walsh", "street":"123 My Street", "city":"Charlottesville", "state":"VA", "zip":"22903"}
+print(address["name"]+"\n"+address["street"]+"\n"+address["city"]+","+address["state"]+ " " + address["zip"])
