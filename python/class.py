53c3b18b5ea64bbb8626bb9c992fa7022c35f767
User <user@vagelos-ve735-0711.apnd.wlan.private.upenn.edu>
2018-06-05 16:08:05 -0400

    name print

---
+++ python/class.py
@@ -0,0 +1,5 @@
+hilt_class = ["name1", "name2", "name3", "Nakho"]
+my_name = "Nakho"
+for name in hilt_class:
+    if name == my_name:
+        print("My name is " + name)
---
+++ python/places.py
@@ -0,0 +1,3 @@
+places =["LA", "Madrid", "Reykavik", "Porto"]
+for wish in places:
+    print ("I would like to visit " + wish )
