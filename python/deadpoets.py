91ebddd05e665f2574e14043f52c7b282d1ab6cb
User <user@vagelos-ve735-0711.apnd.wlan.private.upenn.edu>
2018-06-06 10:09:10 -0400

    bucket test

---
+++ python/deadpoets.py
@@ -0,0 +1,9 @@
+def bucket():
+    print (name + " kicked the bucket in " + year + ".")
+
+poetlist = {'Dickens':'1870','Thackeray':'1863','Trollope':'1882','Hopkins':'1889'}
+
+for name, year in poetlist.items():
+    bucket()
+
+    
